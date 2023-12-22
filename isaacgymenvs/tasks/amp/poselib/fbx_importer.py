0cb1023f00e13399088525e1ffdfa5988ec7daa5
laniakea <akhilsathuluri@gmail.com>
2023-12-22 14:18:54 +0100

    last commit from iit ven 22 dic 2023, 14:18:54, CET

--- isaacgymenvs/tasks/amp/poselib/fbx_importer.py
+++ isaacgymenvs/tasks/amp/poselib/fbx_importer.py
@@ -27,22 +27,24 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import os
 import json
+import os
 
-from poselib.skeleton.skeleton3d import SkeletonTree, SkeletonState, SkeletonMotion
-from poselib.visualization.common import plot_skeleton_state, plot_skeleton_motion_interactive
+from poselib.skeleton.skeleton3d import SkeletonMotion, SkeletonState, SkeletonTree
+from poselib.visualization.common import (
+    plot_skeleton_motion_interactive,
+    plot_skeleton_state,
+)
 
 # source fbx file path
 # fbx_file = "data/01_01_cmu.fbx"
 fbx_file = "data/0x_cmu_silly_dance.fbx"
 
 # import fbx file - make sure to provide a valid joint name for root_joint
-motion = SkeletonMotion.from_fbx(
-    fbx_file_path=fbx_file,
-    root_joint="Hips",
-    fps=60
-)
+motion = SkeletonMotion.from_fbx(fbx_file_path=fbx_file, root_joint="Hips", fps=60)
+
+# to just extract the tree from the fbx file
+tree = SkeletonTree.from_fbx(fbx_file_path=fbx_file, root_joint="Hips")
 
 # save motion in npy format
 # motion.to_file("data/01_01_cmu.npy")
